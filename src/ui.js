244c6c2dbed3b3631c1acb5f4549816a5814515b
Theodoros Pseftelis <pseftelistheo@gmail.com>
2020-04-27 20:06:51 +0300

    tag-render

--- src/ui.js
+++ src/ui.js
@@ -1,13 +1,23 @@
 const parser = new DOMParser();
+/**
+ * 
+ * @param {Array} products 
+ * @returns {void}
+ */
 function renderProducts(products) {
     const container = document.getElementById("product-container");
     container.innerHTML='';
+    let tag_container;
     let dom_string = ``;
     let tags = []; 
+    let uniq_id = '';
     let date = '';
+
     products.forEach(product => {
         tags = product.tags;
         date = product.posttime;
+        uniq_id = 'tag-container-'+ product.id;
+        console.log(`This is the unique id:${uniq_id}`) ;
         
         dom_string = `
         <div  class="col-4 item-product">
@@ -23,19 +33,25 @@ function renderProducts(products) {
             </div><!-- Video-Container END -->
 
             <div class="video-footer row" > 
-                <div class="tag-container col-6">
-                    <button class="btn-tag">Tasty</button>
-                    <button class="btn-tag">Juicy</button>
-                    <button class="btn-tag">Healthy</button>
+                <div id="${uniq_id}" class=" tag-container col-6">
+                  
                 </div>
                 <div class="date-container col-6">${product.posttime}</div>
             </div>
         </div> <!-- Item-Product End -->
         `;
+        let html_product = parser.parseFromString(dom_string, 'text/html'); 
+        container.appendChild(html_product.documentElement);
 
-        let html = parser.parseFromString(dom_string, 'text/html'); 
+        tag_container = document.getElementById(uniq_id);
+        tag_container.innerHTML = '';
+        tags.forEach(tag => {
+            let tag_string = `<button class="btn-tag">${tag}</button>` ; 
+            let html_tag = parser.parseFromString(tag_string, 'text/html'); 
+            tag_container.appendChild(html_tag.documentElement);
+        });
+
         
-        container.appendChild(html.documentElement);
     });
 }
 
